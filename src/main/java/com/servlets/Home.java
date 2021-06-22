214c45dc0f9ad1b3a268501beae867123436f737
YunaH <YunaH503@gmail.com>
2021-06-22 17:45:21 -0400

    Got the servlet to work

--- src/main/java/com/servlets/Home.java
+++ src/main/java/com/servlets/Home.java
@@ -1,63 +1,37 @@
 package com.servlets;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
-// import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.utils.DBConnection;
-
+//@WebServlet("/HelloServlet")
 public class Home extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+    public Home() {
+        super();
 
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String file = "ShowMe.txt";
-    PrintWriter out = response.getWriter();
-
-    try (FileInputStream fis = new FileInputStream(getClass().getClassLoader().getResource(file).getFile());
-        BufferedInputStream bis = new BufferedInputStream(fis)) {
-      // log user info to DB;
-      logToDB(request.getRemoteHost());
-
-      // return content to client
-      response.setContentType("text/html");
-      out.print("<div>hello</div>");
-      out.println("<br/>");
-
-      // read in the file, append the bytes to StringBuilder
-      StringBuilder sb = new StringBuilder();
-      while (bis.available() > 0) {
-        sb.append((char) bis.read());
-      }
-      // display file content
-      out.println("<div>" + sb.toString() + "</div>");
-    } catch (Exception e) {
-      e.printStackTrace();
     }
-    out.close();
-  }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");//setting the content type  
+		PrintWriter pw=response.getWriter();//get the stream to write the data  
+		  
+		//writing html in the stream  
+		pw.println("<html><body>");  
+		pw.println("Welcome to servlet");  
+		pw.println("</body></html>");  
+		  
+		pw.close();//closing the stream
+	}
 
-  private static Boolean logToDB(String info) {
-    String QUERY = "insert into review_exercise_ values(?)";
-    try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(QUERY)) {
-      stmt.setString(1, info);
-      int row = stmt.executeUpdate();
-      if (row > 0) {
-        return true;
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-    return false;
-  }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
 }
